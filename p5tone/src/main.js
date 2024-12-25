ed3183a589a2558b9d64e9477dbc3c7abddee8ef
Kent Campbell <kentmcamp87@gmail.com>
2024-12-25 15:57:39 -0400

    multiple octaves and mouse interaction improvements

--- p5tone/src/main.js
+++ p5tone/src/main.js
@@ -2,29 +2,45 @@ var notes = ["C", "D", "E", "F", "G", "A", "B"];
 var html = "";
 var synth = new Tone.PolySynth().toMaster();
 
-for (var i = 0; i < notes.length; i++) {
-  var hasSharp = true;
-  var note = notes[i];
+for (var octave = 0; octave < 3; octave++) {
 
-  if (note == "E" || note == "B") hasSharp = false;
+  for (var i = 0; i < notes.length; i++) {
+    var hasSharp = true;
+    var note = notes[i];
 
-  html += `<div class='whitenote' onmousedown='noteDown(this)' data-note='${
-    note + "4"
-  }'>`;
+    if (note == "E" || note == "B") hasSharp = false;
 
-  if (hasSharp) {
-    html += `<div class='blacknote' onmousedown='noteDown(this)' data-note='${
-      note + "#4"
-    }'></div>`;
+    html += `<div class='whitenote'
+    onmousedown='noteDown(this, false)'
+    onmouseup='noteUp(this, false)'
+    onmouseleave='noteUp(this, false)'
+    data-note='${
+      note + (octave + 4)
+    }'>`;
+
+    if (hasSharp) {
+      html += `<div class='blacknote'
+      onmousedown='noteDown(this, true)'
+      onmouseup='noteUp(this, true)'
+      onmouseleave='noteUp(this, true)'
+      data-note='${
+        note + "#" + (octave + 4)
+      }'></div>`;
+    }
+    html += "</div>";
   }
-  html += "</div>";
 }
 
 document.getElementById("container").innerHTML = html;
 
-function noteDown(elem) {
+function noteDown(elem, isSharp) {
   var note = elem.dataset.note;
   // alert(note);
+  elem.style.backgroundColor = "cyan";
   synth.triggerAttackRelease(note, "16n");
   event.stopPropagation();
+}
+
+function noteUp(elem, isSharp) {
+  elem.style.background = isSharp ? "black" : "white";
 }
